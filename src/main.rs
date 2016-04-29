65edcae881282aabac45a75bf601c265eef428a2
Jem Tucker <jem.tucker@gmail.com>
2016-04-29 20:48:15 +0100

    Implement parsing of command line argument and loading of source file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,63 @@
+extern crate getopts;
+
 mod interpreter;
 
+use getopts::Options;
 use interpreter::Interpreter;
 
+use std::fs::File;
+use std::io::Read;
+use std::io::Error;
+
 fn main() {
-    let input = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt("s", "source", "Input source file", "PATH");
+    opts.optflag("h", "help", "Print this help menu");
+
+    let parsed = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string())
+    };
+
+    if parsed.opt_str("s").is_some() {
+        let file = parsed.opt_str("s").unwrap();
+        run(&file);
+    } else {
+        print_usage(&args[0], opts);
+        return;
+    }
+}
+
+fn run(file: &str) {
+    match load_file(file) {
+        Ok(prog) => {
+            let mut int = Interpreter::new(prog);
+            int.run();
+        }
+        Err(e) => panic!("{:?}", e)
+    }
+    
+}
+
+fn load_file(path: &str) -> Result<Vec<char>, Error> {
+    let mut file = try!(File::open(path));
+
+    let mut string = String::new();
+
+    try!(file.read_to_string(&mut string));
+
     let mut prog: Vec<char> = Vec::new();
 
-    for c in input.to_string().chars() {
+    for c in string.chars() {
         prog.push(c);
     }
 
-    let mut int = Interpreter::new(prog);
-    int.run();
+    Ok(prog)
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let breif = format!("Usage: {} [options]", program);
+    println!("{}", opts.usage(&breif));
 }
