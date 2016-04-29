a91e453566b7905d7429f10542970ca7bee62fb3
Jem Tucker <jemtucker@Jems-MacBook-Air.local>
2016-04-29 19:33:43 +0100

    Initial commint of BF interpreter

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod interpreter;
+
+use interpreter::Interpreter;
+
+fn main() {
+	let input = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+	let mut prog: Vec<char> = Vec::new();
+
+	for c in input.to_string().chars() {
+		prog.push(c);
+	}
+
+    let mut int = Interpreter::new(prog);
+    int.run();
+}
